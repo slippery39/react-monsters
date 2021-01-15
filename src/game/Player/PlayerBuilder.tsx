18924d2b1879fc7a5a33ffbec9a56e7d194d26c2
Shayne Quinton <shayne_quinton@hotmail.com>
2021-01-15 15:43:59 -0500

    test push from new computer

--- src/game/Player/PlayerBuilder.tsx
+++ src/game/Player/PlayerBuilder.tsx
@@ -1,4 +1,4 @@
-import {GetItem} from "../Items/PremadeItems";
+import { GetItem } from "../Items/PremadeItems";
 import _ from "lodash";
 import { Status } from "../HardStatus/HardStatus";
 import GetPokemon from "../Pokemon/PremadePokemon";
@@ -8,49 +8,50 @@ import { Pokemon } from "game/Pokemon/Pokemon";
 export interface Player {
     name: string,
     pokemon: Array<Pokemon>,
-    currentPokemonId:number,
+    currentPokemonId: number,
     items: Array<Item>
-    id:number
+    id: number
 }
 
 
-export class PlayerBuilder{
+export class PlayerBuilder {
 
-    private player:Player;
+    private player: Player;
 
-    constructor(id:number){
+    constructor(id: number) {
+
         this.player = {
-            id:id, //id's will be assigned by the game itself.
-            name:"",
-            pokemon:[],
-            currentPokemonId:-1, //id's will be assigned by the game itself
-            items:[]
+            id: id, //id's will be assigned by the game itself.
+            name: "",
+            pokemon: [],
+            currentPokemonId: -1, //id's will be assigned by the game itself
+            items: []
         }
     }
-    WithName(name:string) : PlayerBuilder {
+    WithName(name: string): PlayerBuilder {
         this.player.name = name;
         return this;
     }
-    WithPokemon(name:string) : PlayerBuilder{         
-  
+    WithPokemon(name: string): PlayerBuilder {
+
         let pokemon = GetPokemon(name);
         pokemon.id = -1; //gets a new id from the game
         pokemon.status = Status.None
-        this.player.pokemon.push( pokemon );
-        return this;
-    }
-    WithItem(name:string,quantity:number) : PlayerBuilder{
-        const baseItem = GetItem(name);        
-        //item converted for use in the game
-        let newItem : Item = {...baseItem,...{"id":-1,"quantity":quantity}}
-        this.player.items.push(newItem)
-        return this;
-    }
-    WithCustomPokemon(pokemon:Pokemon):PlayerBuilder{
         this.player.pokemon.push(pokemon);
         return this;
     }
-    Build() : Player {
+    WithItem(name: string, quantity: number): PlayerBuilder {
+        const baseItem = GetItem(name);
+        //item converted for use in the game
+        let newItem: Item = { ...baseItem, ...{ "id": -1, "quantity": quantity } }
+        this.player.items.push(newItem)
+        return this;
+    }
+    WithCustomPokemon(pokemon: Pokemon): PlayerBuilder {
+        this.player.pokemon.push(pokemon);
+        return this;
+    }
+    Build(): Player {
         return _.cloneDeep(this.player);
     }
 }
