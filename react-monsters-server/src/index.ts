54ff0e5eb36ee68db1680f86d504a1b0e5e5ae11
Shayne Quinton <shayne_quinton@hotmail.com>
2021-05-29 01:17:49 -0400

    completed most of the game challenging system. started to program in the server battle stuff.
    Signed-off-by: Shayne Quinton <shayne_quinton@hotmail.com>

--- react-monsters-server/src/index.ts
+++ react-monsters-server/src/index.ts
@@ -7,6 +7,7 @@ import { OnNewTurnLogArgs } from '../../react-monsters/src/game/BattleGame';
 import http from "http";
 import { Server, Socket } from "socket.io";
 import bodyParser from 'body-parser';
+import { DefaultEventsMap } from 'socket.io/dist/typed-events';
 
 
 export interface MatchResult {
@@ -33,56 +34,227 @@ app.get("/test", (req, res) => {
     console.log("test recieved!");
     return res.send("Hello world!");
 });
-   
+
 
 const servicesPerRoom: Map<string, BattleService> = new Map<string, BattleService>();
 
 
-interface CustomSocket extends Socket{
-   decoded_token:any //don't know how to remove compiler errors to check this.
-   username:string
+interface CustomSocket extends Socket {
+    decoded_token: any //don't know how to remove compiler errors to check this.
+    username: string
 }
+
+const usernameSocketMap = new Map<string, Socket>();
+
 
 let loggedInUsers: string[] = [];
 
 
-function GetLoggedInUsers(){
+function GetLoggedInUsers() {
     return loggedInUsers;
+}
+
+//We need to store who is currently challenging who
+interface ChallengeRequest {
+    players: string[]
+}
+
+let challenges: ChallengeRequest[] = [];
+
+function FindChallenge(username: string): ChallengeRequest | undefined {
+    return challenges.find(chal => {
+        return chal.players.find(play => play === username) !== undefined;
+    });
+}
+function RemoveChallenge(username:string){
+    _.remove(challenges, (chal => {
+        return chal.players.find((user)=>user===username)!==undefined;
+    }));
+}
+
+interface GameInfo{
+    players:string[],
+    service:BattleService
+ }
+
+ let games: GameInfo[] = [];
+
+function IsInGame(username: string) {
+    return games.find(game=>{
+        return game.players.find(player=>player===username)
+    })!==undefined
+}
+
+function CreateGame(players: string[]) {
+
+    const player1 = new PlayerBuilder()
+        .WithName(players[0])
+        .WithRandomPokemon(6)
+        .Build();
+
+    const player2 = new PlayerBuilder()
+        .WithName(players[1])
+        .WithRandomPokemon(6)
+        .Build();
+
+    let battleService = new BattleService(true);
+    battleService.RegisterPlayer(player1);
+    battleService.RegisterPlayer(player2);
+    battleService.Initialize();
+
+    let gameInfo = {
+        players:[...players],
+        service:battleService 
+    }
+    games.push(gameInfo);
+
+    return gameInfo;
+    
+
+}
+
+async function FindSocketByUserName(username: string){
+    const sockets = await io.sockets.fetchSockets();
+    const socket = sockets.find(sock => {
+        let customSock = (sock as unknown as CustomSocket);
+        return customSock.username === username;
+    });
+    return socket;
 }
 
 io.on("connection", (socket) => {
 
-    let customSocket= (socket as CustomSocket);  
+    let customSocket = (socket as CustomSocket);
 
-    socket.on("login",(username)=>{
+    socket.on("login", (username) => {
         customSocket.username = username;
         console.log(`${customSocket.username} has logged in!`);
+
+        //TODO - redirect to game if user is logged in.
     });
 
-    socket.on("disconnect",()=>{     
-       _.remove(loggedInUsers,(user)=>user===customSocket.username);
-       io.sockets.emit("users-changed",loggedInUsers);
+    socket.on("disconnecting", () => {
+        console.log(customSocket.username + " has disconnected");
+        _.remove(loggedInUsers, (user) => user === customSocket.username);
+        RemoveChallenge(customSocket.username);
+
+        console.log("Challenge Length",challenges.length);
+        io.sockets.emit("users-changed", loggedInUsers);
     });
 
-    socket.on("challenge-request",async (challengeOptions)=>{
+    socket.on("challenge-request", async (challengeOptions) => {
         console.log(challengeOptions.player1);
         console.log(challengeOptions.player2);
-        //Get socket id of player2.
-        const sockets = await io.sockets.fetchSockets();
 
-       const player2Socket =  sockets.find(sock=>{
-            let customSock = (sock as unknown as CustomSocket);
-            return customSock.username === challengeOptions.player2;
-        });
+        const {player1,player2} = challengeOptions;
 
-        if (player2Socket === undefined){
-            console.error("Could not find socket for challenge request",challengeOptions.player2);
+        if (FindChallenge(player1) !== undefined || IsInGame(player1)){
+            //should not be able to issue more than 1 challenge
             return;
         }
-        console.log("emitting event");
-        player2Socket.emit("challenge-request-received",challengeOptions);          
+        //Get socket id of player2.
+        const sockets = await io.sockets.fetchSockets();
+        const challengeRequest: ChallengeRequest = {
+            players: []
+        }
+        const player1Socket = await FindSocketByUserName(player1);
+        if (player1Socket === undefined) {
+            console.error("Could not find socket for challenge request", player1);
+            return;
+        }
+
+        const player2Socket = await FindSocketByUserName(player2);
+
+        if (player2Socket === undefined) {
+            console.error("Could not find socket for challenge request", player2);
+            return;
+        }
+
+        if (FindChallenge(player2) !== undefined || IsInGame(player2)) {
+            player1Socket.emit("challenge-request-error", { message: `${challengeOptions.player2} cannot be challenged at the moment!` })
+            return;
+        }
+
+        //this should be removed once the challenge is accepted.
+        challenges.push({
+            players: [challengeOptions.player1, challengeOptions.player2]
+        });
+
+        player1Socket.emit("challenge-request-sent", challengeOptions);
+        player2Socket.emit("challenge-request-received", challengeOptions);
     });
- 
+
+    socket.on("challenge-request-accept", async (options) => {
+        //challenge has been accepted, remove the challenge and put them both into a game.
+        const challenge = FindChallenge(customSocket.username);
+
+        if (challenge === undefined){
+            console.error(`Could not find challenge :(`);
+            return;
+        }
+
+        const player1Socket = await FindSocketByUserName(challenge.players[0]);
+        const player2Socket = await FindSocketByUserName(challenge.players[1]);
+
+        player1Socket?.emit("challenge-ready");
+        player2Socket?.emit("challenge-ready");
+
+        console.log("challenge accepted");
+        //emit a message to both players
+        RemoveChallenge(customSocket.username);
+        const gameInfo = CreateGame(challenge.players); 
+        const battleService = gameInfo.service;
+        player1Socket?.emit("match-begin",{
+            players:gameInfo.players,
+            myId:gameInfo.service.GetPlayers()[0].id,
+            myName:gameInfo.service.GetPlayers()[0].name
+        })
+        player2Socket?.emit("match-begin",{
+            players:gameInfo.players,
+            myId:gameInfo.service.GetPlayers()[1].id,
+            myName:gameInfo.service.GetPlayers()[1].name
+        });
+        
+        socket.on("action", (action) => {
+            console.log("action recieved", action);
+            battleService?.SetPlayerAction(action);
+        });
+
+        battleService.OnNewTurnLog.on((args: OnNewTurnLogArgs) => {
+            console.log("emitting new turn log", args.currentTurnLog.length);
+            player1Socket?.emit("newturnlog", args);
+            player2Socket?.emit("newTurnLog",args)
+        });
+
+        battleService.OnGameOver.on((args) => {
+            player1Socket?.emit("gameover",args);
+            player2Socket?.emit("gameover",args);
+        });
+
+        socket.on("gamestartready", (data) => {
+            battleService!.Start();
+            player1Socket?.emit("gamestart", { field: battleService.GetField() });
+            player2Socket?.emit("gamestart", { field: battleService.GetField() });
+        });     
+        
+        //todo: put into game.
+    });
+
+  
+
+    socket.on("challenge-request-decline",async ()=>{
+        const challenge = FindChallenge(customSocket.username);
+        if (challenge === undefined){
+            console.error(`Could not find challenge :(`);
+            return;
+        }
+        const player1Socket = await FindSocketByUserName(challenge.players[0]);
+        const player2Socket = await FindSocketByUserName(challenge.players[1]);
+
+        player1Socket?.emit("challenge-request-declined");
+        RemoveChallenge(customSocket.username);
+    });
+
     //Below here shouldn't matter anymore.
     socket.on("testgame", () => {
         console.log("test game has been started");
@@ -140,9 +312,6 @@ io.on("connection", (socket) => {
 
             battleService.Initialize();
 
-            //this is the problem right here.
-
-            console.log(battleService.GetField());
             io.to(room).emit("gamestart", { field: battleService.GetField() });
             console.log("game starting");
         }
@@ -173,25 +342,38 @@ io.on("connection", (socket) => {
 })
 
 
-app.get("/getOnlineUsers",async(req,res)=>{
-    return res.json({users:loggedInUsers})
-}); 
+app.get("/getOnlineUsers", async (req, res) => {
+    return res.json({ users: loggedInUsers })
+});
 
 
-app.post('/login',async(req,res)=>{    
+app.post('/login', async (req, res) => {
     console.log("login request recieved");
     console.log(req.body);
     //fail
-    if (loggedInUsers.find(user=>user===req.body.name)!==undefined){
+    if (loggedInUsers.find(user => user === req.body.name) !== undefined) {
         return res.status(401).send({
             message: 'Connection Failed - Username already connected!'
-         });
+        });
     }
-    else{ //success
+    else { //success
         loggedInUsers.push(req.body.name)
-        io.sockets.emit("users-changed",loggedInUsers);
-        return res.json({status:"success",username:req.body.name});
+        io.sockets.emit("users-changed", loggedInUsers);
+        return res.json({ status: "success", username: req.body.name });
     }
+});
+
+app.get("getvalidactions",async(req,res)=>{
+    const username = req.params.username;
+    const gameInfo = games.find(info=>info.players.find(name=>name==username)!==undefined);
+    const playerInGame= gameInfo?.service.GetPlayers().find(player=>player.name === username);
+
+    if (playerInGame === undefined){
+        return;
+    }
+
+    const validActions = await gameInfo?.service.GetValidActions(playerInGame.id);
+    return res.json(validActions);
 });
 
 /* -> Marked For Deletion.
--- react-monsters/src/components/NetworkPlay/MainLobby.tsx
+++ react-monsters/src/components/NetworkPlay/MainLobby.tsx
@@ -1,4 +1,5 @@
-import { Card, message } from "antd"
+import { Card, message, Modal } from "antd"
+import { modalGlobalConfig } from "antd/lib/modal/confirm";
 
 import React, { useEffect, useState } from "react"
 import { NetworkInfo } from "./NetworkPlayController";
@@ -37,32 +38,95 @@ const MainLobby = (props: Props) => {
 
             setOnlinePlayers(resJSON.users);
         }
-        )();
+            )();
 
-        props.networkInfo.socket!.on("users-changed", handleUsersChanged);
+            props.networkInfo.socket!.on("users-changed", handleUsersChanged);
 
-        return () => {
-            props.networkInfo.socket!.off("users-changed", handleUsersChanged);
+            return () => {
+                props.networkInfo.socket!.off("users-changed", handleUsersChanged);
+            }
+
+        }, []);
+
+        useEffect(() => {
+
+            if (props.networkInfo.socket === undefined){
+                console.error(`something went wrong, socket is undefined`);
+                throw new Error(`something went wrong, socket is undefined`);
+            }
+
+            props.networkInfo.socket.on("match-begin",(info)=>{
+                message.info("Match is beginning");
+                console.log(info);
+            });     
+
+            let challengeRecievedModal:any | undefined = undefined;
+
+            const handleAccept = ()=>{
+                console.log("trying to handle accept");
+                props.networkInfo.socket!.emit("challenge-request-accept");
+                console.log("player request recieved");
+                challengeRecievedModal?.destroy();
+            }
+
+            const handleDecline = ()=>{
+                props.networkInfo.socket!.emit("challenge-request-decline");
+                console.log("player request declined");
+                challengeRecievedModal?.destroy();
+            }
+
+            props.networkInfo.socket!.on("challenge-request-received", (options: { player1: string, player2: string }) => {
+   
+                challengeRecievedModal = Modal.confirm({
+                    content: `${options.player1} has challenged you to a battle`,
+                    title: `Battle Request`,
+                    okText: 'Accept',
+                    onOk:()=>handleAccept(),
+                    cancelText: 'Decline',
+                    onCancel:()=>handleDecline()
+                }
+                );                
+               
+                //close if the challenge is void        
+                // message.info(`${options.player1} has challenged you to a battle!`)
+            });
+
+
+            let waitingForResponseModal:any |undefined;
+            props.networkInfo.socket!.on("challenge-request-sent", (options: { player1: string, player2: string })=>{
+               
+                waitingForResponseModal = Modal.info({
+                    content: `Waiting for ${options.player2} to respond`,
+                    title: `Battle Request`,    
+                    cancelButtonProps:{disabled:true,style:{display:'none'}},
+                    okButtonProps:{disabled:true,style:{display:'none'}}                 
+                    });   
+            });
+
+            props.networkInfo.socket!.on("challenge-ready",()=>{
+                waitingForResponseModal?.destroy();
+                message.info(`Challenge has been accepted`);                        
+            });
+
+            props.networkInfo.socket!.on("challenge-request-declined",()=>{
+                waitingForResponseModal?.destroy();
+                message.warn("Challenge has been declined");
+            });
+    
+        }, []);
+
+        const createPlayerInfo = (player: string): NetworkPlayerInfo => {
+            return {
+                name: player,
+                onlineStatus: onlinePlayers.includes(player) ? NetworkPlayerStatus.Online : NetworkPlayerStatus.Offline
+            }
         }
 
-    }, []);
-
-    useEffect(() => {
-        props.networkInfo.socket!.on("challenge-request-received", (options) => {
-            message.info(`${options.player1} has challenged you to a battle!`)
-        });
-    }, []);
-
-    const createPlayerInfo = (player:string):NetworkPlayerInfo=>{
-        return {
-            name:player,
-            onlineStatus: onlinePlayers.includes(player) ? NetworkPlayerStatus.Online : NetworkPlayerStatus.Offline
-        }
-    }
-
-    const otherPlayerList = onlinePlayers
+        const otherPlayerList = onlinePlayers
         .filter(player => player !== props.networkInfo.currentPlayer)
         .map(player => <PlayerProfile onChallengeClick={onChallengeClick} player={createPlayerInfo(player)}></PlayerProfile>)
+
+
 
 
     return (<Card>
