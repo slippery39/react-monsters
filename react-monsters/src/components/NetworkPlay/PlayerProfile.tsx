280783f51f50a95f06ca3c46cea79207445fae52
Shayne Quinton <shayne_quinton@hotmail.com>
2021-06-08 01:03:06 -0400

    color coded online status on player profile

--- react-monsters/src/components/NetworkPlay/PlayerProfile.tsx
+++ react-monsters/src/components/NetworkPlay/PlayerProfile.tsx
@@ -1,6 +1,7 @@
 import { Button, Card } from "antd";
 import React from "react"
 import NetworkPlayerInfo, { NetworkPlayerStatus } from "./NetworkPlayer";
+import "./PlayerProfile.css";
 
 interface Props {
     player:NetworkPlayerInfo,
@@ -9,9 +10,19 @@ interface Props {
 
 
 const PlayerProfile = (props: Props) => {
+
+    let onlineStatusClass = "";
+
+    if (props.player.onlineStatus === NetworkPlayerStatus.Online){
+        onlineStatusClass = "player-profile-online";
+    }
+    if (props.player.onlineStatus === NetworkPlayerStatus.InGame){
+        onlineStatusClass = "player-profile-in-game";
+    }
+
     return (<Card>
         <div>{props.player.name}</div>
-        <div>{props.player.onlineStatus}</div>
+        <div className={onlineStatusClass}>{props.player.onlineStatus}</div>
         {props.player.onlineStatus!== NetworkPlayerStatus.InGame &&<div><Button onClick={()=>props.onChallengeClick(props.player.name)}>Challenge!</Button></div>
     }    </Card>)
 }
