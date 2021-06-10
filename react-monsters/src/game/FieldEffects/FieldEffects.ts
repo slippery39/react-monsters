c03b22ddb88fe5328b796478e12cac08cd3c1cf4
Shayne Quinton <shayne_quinton@hotmail.com>
2021-06-10 17:56:30 -0400

    minor argument name change
    Signed-off-by: Shayne Quinton <shayne_quinton@hotmail.com>

--- react-monsters/src/game/FieldEffects/FieldEffects.ts
+++ react-monsters/src/game/FieldEffects/FieldEffects.ts
@@ -25,22 +25,22 @@ export class WishFieldEffect extends FieldEffect {
     count: number = 1;
     healingAmount: number = 1;
 
-    OnCreated(turn: IGame, player: Player) {
+    OnCreated(game: IGame, player: Player) {
         this.playerId = player.id;
         const pokemon = GetActivePokemon(player);
         this.healingAmount = pokemon.originalStats.hp / 2;
-        turn.AddMessage(`${GetActivePokemon(player).name} made a wish!`)
+        game.AddMessage(`${GetActivePokemon(player).name} made a wish!`)
     }
 
 
-    EndOfTurn(turn: IGame, pokemon: Pokemon) {
+    EndOfTurn(game: IGame, pokemon: Pokemon) {
         if (this.count < 2) {
             this.count++;
         }
         else {
-            turn.ApplyHealing(pokemon, this.healingAmount);
-            turn.AddMessage(`${pokemon.name}'s wish came true!`);
-            _.remove(turn.field.fieldEffects!, (fe => fe === this));
+            game.ApplyHealing(pokemon, this.healingAmount);
+            game.AddMessage(`${pokemon.name}'s wish came true!`);
+            _.remove(game.field.fieldEffects!, (fe => fe === this));
         }
     }
 }
